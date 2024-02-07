696ef0fbc0e7842f2764103f2c96746e1500fdba
Andrej Tasevski Mestrovic <andrejmtasevski@gmail.com>
2024-02-07 15:15:27 +0100

    Solution for a

---
+++ RC Solution 1/Controllers/EmployeeController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RC_Solution_1.Models;
+using System.Drawing;
+using System.Text.Json.Serialization;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace RC_Solution_1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Fetch all records from the API and group them by employee name
+            List<EmployeeModel>? employees = GroupRecordsByEmployeeName(GetAllRecords());
+
+            // Order the records by the total time worked in descending order
+            employees = OrderRecordsByTimeWorked(employees, "desc");
+
+            return View(employees);
+        }
+
+        /// <summary>
+        /// Fetch all records from the API
+        /// </summary>
+        /// <returns>Full list of employee records</returns>
+        private List<EmployeeModel>? GetAllRecords()
+        {
+            HttpClient client = new HttpClient();
+            string url = "https://rc-vault-fap-live-1.azurewebsites.net/api/gettimeentries?code=vO17RnE8vuzXzPJo5eaLLjXjmRW07law99QTD90zat9FfOQJKKUcgQ==";
+            HttpResponseMessage result = client.GetAsync(url).Result;
+            List<EmployeeModel>? allEmployees = JsonConvert.DeserializeObject<List<EmployeeModel>>(result.Content.ReadAsStringAsync().Result);
+
+            if (allEmployees != null)
+                allEmployees.ForEach(e => e.calculateTotalTimeWorked());
+
+            return allEmployees;
+        }
+
+        /// <summary>
+        /// Groups the records by employee name and sums the total time worked
+        /// </summary>
+        /// <param name="employees">Full list of employee records</param>
+        /// <returns>Grouped list</returns>
+        private List<EmployeeModel> GroupRecordsByEmployeeName(List<EmployeeModel>? employees)
+        {
+            if (employees == null)
+                return new List<EmployeeModel>();
+
+            return employees.GroupBy(e => e.EmployeeName).Select(e => new EmployeeModel
+            {
+                Id = e.First().Id,
+                StarTimeUtc = e.First().StarTimeUtc,
+                EndTimeUtc = e.First().EndTimeUtc,
+                EntryNotes = e.First().EntryNotes,
+                EmployeeName = e.First().EmployeeName,
+                TotalTimeWorkedHours = e.Sum(e => e.TotalTimeWorkedHours)
+            })
+                .Where(e => !String.IsNullOrEmpty(e.EmployeeName))
+                .ToList();
+        }
+
+        /// <summary>
+        /// Orders the records by the total time worked
+        /// </summary>
+        /// <param name="employees"></param>
+        /// <param name="direction">asc | desc</param>
+        /// <returns></returns>
+        private List<EmployeeModel> OrderRecordsByTimeWorked(List<EmployeeModel>? employees, string direction)
+        {
+            if (employees == null)
+                return new List<EmployeeModel>();
+
+            if (direction == "asc")
+                return employees.OrderBy(e => e.TotalTimeWorkedHours).ToList();
+            else
+                return employees.OrderByDescending(e => e.TotalTimeWorkedHours).ToList();
+        }
+    }
+}
---
+++ RC Solution 1/Models/EmployeeModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualBasic;
+using System.Text.Json.Serialization;
+
+namespace RC_Solution_1.Models
+{
+    // Example data:
+    // "Id": "25768d9f-502a-4776-8796-c26d76d6a6eb",
+    // "EmployeeName": "Abhay Singh",
+    // "StarTimeUtc": "2022-02-22T14:15:00",
+    // "EndTimeUtc": "2022-02-22T12:01:00",
+    // "EntryNotes": "working on project Take-Two Interactive",
+    // "DeletedOn": null
+    public class EmployeeModel
+    {
+        private Guid _id;
+        private string _employeeName;
+        private DateTime _starTimeUtc;
+        private DateTime _endTimeUtc;
+        private string _entryNotes;
+        private string? _deleteon;
+        private double? _totalTimeWorked;
+
+        public EmployeeModel()
+        {
+            EmployeeName = "";
+            StarTimeUtc = DateTime.Now;
+            EndTimeUtc = DateTime.Now;
+            EntryNotes = "";
+            Deleteon = null;
+        }
+
+        public EmployeeModel(string employeeName, DateTime startTimeUtc, DateTime endTimeUtc, string entryNotes, string? deleteon)
+        {
+            EmployeeName = employeeName;
+            StarTimeUtc = startTimeUtc;
+            EndTimeUtc = endTimeUtc;
+            EntryNotes = entryNotes;
+            Deleteon = deleteon;
+        }
+
+        public void calculateTotalTimeWorked()
+        {
+            TotalTimeWorkedHours = Math.Abs(EndTimeUtc.Subtract(StarTimeUtc).TotalHours); 
+        }
+
+        public Guid Id { get => _id; set => _id = value; }
+        public string EmployeeName { get => _employeeName; set => _employeeName = value; }
+        public DateTime StarTimeUtc { get => _starTimeUtc; set => _starTimeUtc = value; }
+        public DateTime EndTimeUtc { get => _endTimeUtc; set => _endTimeUtc = value; }
+        public string EntryNotes { get => _entryNotes; set => _entryNotes = value; }
+        public string? Deleteon { get => _deleteon; set => _deleteon = value; }
+        public double? TotalTimeWorkedHours { get => _totalTimeWorked; set => _totalTimeWorked = value; }
+    }
+}
